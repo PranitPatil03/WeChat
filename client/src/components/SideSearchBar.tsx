cdf7555de3e6aff6c19a4dd8225e45b039513a13
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-03-11 00:11:44 +0530

    User Search UI Completed

--- client/src/components/SideSearchBar.tsx
+++ client/src/components/SideSearchBar.tsx
@@ -73,39 +73,24 @@ const SideSearchBar = () => {
                 </SheetTrigger>
                 <SheetContent side={side}>
                   <SheetHeader>
-                    <SheetTitle>Edit profile</SheetTitle>
-                    <SheetDescription>
-                      Make changes to your profile here. Click save when you're
-                      done.
-                    </SheetDescription>
+                    <SheetTitle>Search User</SheetTitle>
                   </SheetHeader>
+                        <hr className="border-1 border-lightGrey/40 mt-3"></hr>
+
                   <div className="grid gap-4 py-4">
                     <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="name" className="text-right">
-                        Name
-                      </Label>
                       <Input
                         id="name"
-                        value="Pedro Duarte"
-                        className="col-span-3"
+                        placeholder="Enter Username or Name"
+                        className="col-span-3 "
                       />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="username" className="text-right">
-                        Username
-                      </Label>
-                      <Input
-                        id="username"
-                        value="@peduarte"
-                        className="col-span-3"
-                      />
+                      <SheetHeader>
+                        <SheetHeader>
+                          <Button type="submit">Search</Button>
+                        </SheetHeader>
+                      </SheetHeader>
                     </div>
                   </div>
-                  <SheetFooter>
-                    <SheetClose asChild>
-                      <Button type="submit">Save changes</Button>
-                    </SheetClose>
-                  </SheetFooter>
                 </SheetContent>
               </Sheet>
             ))}
